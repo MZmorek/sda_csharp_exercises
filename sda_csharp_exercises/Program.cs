092dd4784390c5867aef8d6c7bef1e6ebe5dab11
MZmorek <malgorzata.zmorek@gmail.com>
2022-05-14 11:01:14 +0200

    E04 progress

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -6,6 +6,30 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            DrawTriangle();
+        }
+        static void DrawTriangle()
+        {
+            int numberOfLines = 3;
+            Console.WriteLine("Wpisz znak: ");
+            char sign = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(sign);
+                }
+                Console.WriteLine();
+            }
+            for (int i = numberOfLines; i > 0; i--)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write(sign);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }

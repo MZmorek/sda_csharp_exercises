72fba08bd89323f51f501f0ed492f1c43edb707c
MZmorek <malgorzata.zmorek@gmail.com>
2022-05-14 10:38:21 +0200

    E03 done

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -6,6 +6,19 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            int length = 4;
+            int firstElement = 2;
+            int difference = 3;
+            ComputeArithmeticSequence(length, firstElement, difference);
+        }
+        static void ComputeArithmeticSequence(int length, int firstElement, int difference)
+        {
+            int currentElement = firstElement;
+            for(int i = 1; i <= length; i++)
+            {
+                Console.WriteLine(currentElement);
+                currentElement += difference;
+            }
         }
     }
 }

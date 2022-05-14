06dbdc32ea0e466b80600f5fa6f04ac50e2c4833
MZmorek <malgorzata.zmorek@gmail.com>
2022-05-14 13:51:19 +0200

    E08 done

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace sda_csharp_exercises
 {
@@ -6,6 +7,38 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Solution1();
+            Console.WriteLine(" ");
+            Solution2();
+        }
+        static void Solution1()
+        {
+            int[] numbers = { 2, 5, 1, 2, 5 };
+
+            int[] result = numbers.Distinct().ToArray();
+            foreach (int num in result)
+            {
+                Console.Write(num);
+            }
+        }
+        static void Solution2()
+        {
+            int[] numbers = { 2, 5, 1, 2, 5 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        numbers[j] = -1;
+                    }
+                }
+                if (numbers[i] >= 0)
+                {
+                    Console.Write(numbers[i]);
+                }
+            }
         }
     }
 }

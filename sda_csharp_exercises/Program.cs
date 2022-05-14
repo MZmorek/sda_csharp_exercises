4da25b0384c830c31f0e1555c9cc23f3a61b1853
MZmorek <malgorzata.zmorek@gmail.com>
2022-05-14 09:55:16 +0200

    E01 done

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -5,7 +5,27 @@ namespace sda_csharp_exercises
     class Program
     {
         static void Main(string[] args)
+            
         {
+            try
+            {
+                Console.WriteLine("Podaj cenę produktu:");
+                int price = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Podaj liczbę produktów:");
+                int productNumber = Convert.ToInt32(Console.ReadLine());
+                PriceList(price, productNumber);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void PriceList(int price, int productNumber)
+        {
+            for (int i = 1; i <= productNumber; i++)
+            {
+                Console.WriteLine($"Liczba sztuk: {i}, łączna cena {i * price} zł.");
+            }
         }
     }
 }

f164638704fcb2941bab1e546478c48516808f40
MZmorek <malgorzata.zmorek@gmail.com>
2022-05-15 15:42:31 +0200

    E20 done

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 
-namespace sda_csharp_exercises
+namespace sda_csharp_exercises_classes
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Account account = new();
+            account.IncreaseBalance(100);
+            SavingAccount savingAccount = new();
+            savingAccount.IncreaseBalance(200);
+            savingAccount.IncreaseInterests(0.20M);
+            Bank bank = new();
+            bank.AddAccount(account);
+            bank.AddAccount(savingAccount);
+            Console.WriteLine($"Total amount: {bank.GetTotalAmount()}");
         }
     }
 }

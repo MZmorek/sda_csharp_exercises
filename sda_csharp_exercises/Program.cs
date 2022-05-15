09d2ccb4ee73f789104fcbddbd862485b49826fb
MZmorek <malgorzata.zmorek@gmail.com>
2022-05-15 09:01:38 +0200

    E10 done

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -6,6 +6,34 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 5, 2, 8, 1, 0, 3 };
+            PrintArray(BubbleSort(numbers));
+        }
+        static void PrintArray(int[] numbers)
+        {
+            foreach (int i in numbers)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+        static int[] BubbleSort(int[] numbers)
+        {
+            int temporary = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+
+                        temporary = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = temporary;
+                    }
+                }
+            }
+            return numbers;
         }
     }
 }

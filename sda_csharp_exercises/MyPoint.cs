945e2ce930518ff6fc6d09498d6b5e2308f2dbbd
MZmorek <malgorzata.zmorek@gmail.com>
2022-05-21 11:36:28 +0200

    E23 done

---
+++ sda_csharp_exercises/MyPoint.cs
@@ -0,0 +1,19 @@
+﻿namespace sda_csharp_exercises
+{
+    public class MyPoint
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public MyPoint(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{X}, {Y}";
+        }
+    }
+}
--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -6,6 +6,17 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            MyPoint point = new(5, 10);
+            Console.WriteLine(point);
+
+            MyPoint anotherPoint = new(3, 2);
+            Console.WriteLine(anotherPoint);
+
+            LineSegment firstLine = new(point, anotherPoint);
+            Console.WriteLine(firstLine.GetLength());
+
+            LineSegment secondLine = new(5, 10, 3, 2);
+            Console.WriteLine(secondLine.GetLength());
         }
     }
 }
